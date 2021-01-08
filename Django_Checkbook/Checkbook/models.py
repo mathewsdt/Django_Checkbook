80a89b07533f53fd91ff35ab71a8ea349bbe4ec8
Dom Mathews <mathews.dom@yahoo.com>
2021-01-07 17:31:40 -0700

    Initial commit

---
+++ Django_Checkbook/Checkbook/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    initial_deposit = models.DecimalField(max_digits=15, decimal_places=2)
+
+    Accounts = models.Manager()
+
+    # Allows refrences to a specific account be returned
+    # as the owner's name not the primary key
+    def __str__(self):
+        return self.first_name + '' + self.last_name
+
+TransactionTypes = [('Deposit', 'Deposit'), ('withdrawel', 'withdrawl')]
+
+class Transaction(models.Model):
+    data = models.DateField()
+    type = models.CharField(max_length=10, choices=TransactionTypes)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=100)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    Transaction = models.Manager()
+
